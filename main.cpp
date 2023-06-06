eaee5e0e2eff8ab9fc5eecb183d09d2273350feb
DeonaKoci <127995793+DeonaKoci@users.noreply.github.com>
2023-06-06 21:42:45 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
 
+/*
+using namespace std;
+
+int main() {
+    char nomi[5][15], cognomi[5][15];
+    init(nomi);
+    init(cognomi);
+    inserimento(nomi, cognomi);
+
+    char nome[15], cognome[15];
+    inserimentonome(nome, 15);
+    inserimentocognome(cognome, 15);
+
+    int id = ricerca(nomi, cognomi, nome, cognome);
+    if (id != -1) {
+        cout << "Trovato in posizione " << id << endl;
+    }
+    else {
+        cout << "Non trovato" << endl;
+    }
+
+    return 0;
+}*/
+
 #include <iostream>
 #include "lib.h"
 using namespace std;
@@ -13,8 +37,8 @@ int main() {
         for(int j=0;j<20;j++) {
             a[j] = ' ';
             lista[i][j] = ' ';
-        }
-    }
+        
+    }}
 
     for(int i=0;i<10;i++){
         cin >> lista[i];
@@ -25,7 +49,7 @@ int main() {
         cout << "trovato in posizione "<< pos;
     }else{
         cout << "non trovato";
-    }
+    }}
 
     return 0;
 }
