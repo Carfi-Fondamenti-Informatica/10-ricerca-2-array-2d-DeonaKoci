a7ea7dd8a8cf47e63d7d9c7f1ba977396c5ee36c
DeonaKoci <127995793+DeonaKoci@users.noreply.github.com>
2023-06-06 21:38:54 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+bool cerca(char listanome[10][20], int &pos, char a[]){
+    for(int i=0;i<10;i++){
 
+        for(int j=0;j<20;j++){
+            if(listanome[i][j] != a[j]){
+                  j = 20;
+            }else{
+
+                    if(j==19){
+                          pos = (i);
+
+                           return true;
+
+                    }}}
+    }
+    return false;
+}
