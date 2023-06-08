57329a475c3e4f39c2dc981af2496fc164c2418e
DeonaKoci <127995793+DeonaKoci@users.noreply.github.com>
2023-06-08 21:54:28 +0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,14 @@
-bool cerca(char[10][20], int &, char[]);
+//bool cerca(char[10][20], int &, char[]);
+
+/*
+bool funzione (char [][20], char[][20], int &contatore);
+
+*/
+
+
+
+void init (char mat[], int n);
+void init(char mat[5][15]);
+void inserimento(char nomi[5][15], char cognomi[5][15]);
+void inserimento(char mat[], int nomi);
+int ricerca(char nomi[5][15], char cognomi[5][15], char nome[15], char cognome[15]);
